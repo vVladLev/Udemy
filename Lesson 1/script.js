e8234c7c30f163f35cb79eaa78aedf6b5d0185b6
Linero <vladyshalevchuk@gmail.com>
2024-05-10 12:36:25 -0600

    Commit before lesson 24 JS.1

--- Lesson 1/script.js
+++ Lesson 1/script.js
@@ -1,19 +1,61 @@
-const numberOfFilms = +prompt("How many movies did your watch?", '');
+// console.log( NaN || 2 || undefined );
 
-const personalMovieDB = {
-   count: numberOfFilms,
-   movies: {},
-   actors: {},
-   genres: [],
-   private: false
-};
 
-const a = prompt("What the name of one of them?", ''),
-      b = prompt("How will you rate it?", ''),
-      c = prompt("What the name of another one?", ''),
-      d = prompt("How will you rate it?", '');
+// console.log( NaN && 2 && undefined );
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log( 1 && 2 && 3 );
+
+
+// console.log( !1 && 2 || !3 );
+
+
+// console.log( 25 || null && !3 );
+
+
+// console.log( NaN || null || !3 || undefined || 5);
+
+
+// console.log( NaN || null && !3 && undefined || 5);
+
+
+// console.log( 5 === 5 && 3 > 1 || 5);
+
+for (i = 5; i < 10; i++) {
+   console.log(i);
+}
+
+for (i = 20; i >= 10; i--) {
+   if (i === 13) {
+      break;
+   }
+   console.log(i);
+}
+
+for (i = 2; i <= 10; i++) {
+   if(i%2==0) console.log(i);
+   
+}
+
+let q = 2;
+
+while(q<=16){
+   if(q%2==1){
+      console.log(q);
+   }
+   q++;
+   
+}
+let qq = [] 
+for(let i=5; i<= 10; i++){
+   for(let j=0;j<=10;j++){
+      qq[j]=[i];
+      console.log(qq[j])
+      break;
+   }
+   
+}
+    // Пишем решение вот тут
+      for(let i=0; i<=10; i++){\
+        console.log(i);
+    }
